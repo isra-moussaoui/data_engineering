5fe3600f7ac1b5b76823e60abaf3b1fc80600d66
Izz-b <isramoussaoui01@gmail.com>
2026-04-16 17:19:15 +0100

    Update pipeline config and docs

--- test_pg.py
+++ test_pg.py
@@ -1,11 +1,11 @@
 import psycopg
 
-conn = psycopg.connect(
-    host="127.0.0.1",
-    port=5432,
-    dbname="currency_db",
-    user="postgres",
-    password="postgres"
+cconn = psycopg.connect(
+    host=os.getenv("POSTGRES_HOST", "127.0.0.1"),
+    port=os.getenv("POSTGRES_PORT", "5432"),
+    dbname=os.getenv("POSTGRES_DB", "currency_db"),
+    user=os.getenv("POSTGRES_USER", "postgres"),
+    password=os.getenv("POSTGRES_PASSWORD", "postgres")
 )
 
 print("Connected successfully")
